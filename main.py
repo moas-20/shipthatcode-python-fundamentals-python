fcaf316d4e2974e6df1b35232c3f97b56a45e645
moas-20 <mk333hot@gmail.com>
2026-09-16 09:12:23 +0200

    passed 06 test

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-num = int(input())
+score = int(input())
 
-if num > 0 :
-    print("positive")
-if num < 0 :
-    print("negative")
-
-if num == 0 :
-    print("zero")
+if score >= 90 :
+    print("A")
+elif score >= 80 :
+    print("B")
+elif score >= 70 :
+    print("C")
+elif score >= 60 :
+    print("D")
+else :
+    print("F")
